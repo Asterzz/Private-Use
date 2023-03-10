abeb8a7d028db34f6a8471688ac6f7a1ff9eb758
Asterzz <2774177908@qq.com>
2023-03-10 17:51:55 +0800

    v0.1.1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Private-Use
+# Private-Use #
+***
+*感谢EasyChatGPT-API项目*
+***
+## V 0.1.0
+- Learn how to deploy this project to a cloud server
+## V 0.1.1
+- Learn how to prettify the front interface
+- Learn how to connect history data to MySQL
+- Learn how to show history data by user ip
+- fix the problem with html code
+
+***
+*continuning*
--- app.py
+++ app.py
@@ -4,19 +4,54 @@ import openai
 import markdown
 import markdown.extensions.fenced_code
 import markdown.extensions.codehilite
+import pymysql
+import time
+import json
+import os
 
-openai.api_key = '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'
+# SET PATH
+file_path = os.path.abspath(__file__)
+dir_path = os.path.dirname(file_path)
+os.chdir(dir_path)
+
+# READ CONFIG
+with open("db_config.json", "r") as json_file:
+    db_config = json.load(json_file)
+
+#sql connection
+connection = pymysql.connect(
+    host=db_config["host"],
+    port=db_config["port"],
+    user=db_config["user"],
+    password=db_config["password"],
+    db=db_config["db"]
+)
+
+
+openai.api_key = db_config["api_key"]
 app = Flask(__name__)
 messages = []
+
+
 @app.route('/')
 def home():
-
+    
     return render_template('index.html')
 
 @app.route('/get_response', methods=['POST'])
-
 def get_bot_response():
+    cursor = connection.cursor()
     user_input = request.form['user_input']
+    # get user ip
+    user_ip = request.remote_addr
+    # print(f'==={user_ip}')
+
+    # get user iput time
+    timestamp = time.time()
+    localtime = time.localtime(timestamp)
+    formatted_time = time.strftime("%Y-%m-%d %H:%M", localtime)
+    # print(f'==={formatted_time}')
+
     # print(user_input)
     messages.append({'role': 'user', 'content': user_input})
     completion = openai.ChatCompletion.create(
@@ -24,14 +59,39 @@ def get_bot_response():
         messages=messages
     )
     ai_response = completion.choices[0].message['content']
+
     # print(ai_response)
     messages.append({'role': 'assistant', 'content': ai_response})
     print(messages)
+
+    # ADD DATA INTO SQL
+    insert_query = "INSERT INTO user_info (ip, user_time, user_input, gpt_output) VALUES (%s, %s, %s, %s)"
+    values = (user_ip, formatted_time, user_input, ai_response)
+    cursor.execute(insert_query, values)
+    connection.commit()
+    cursor.close()
+
     return  Markup(markdown.markdown(ai_response, extensions=['fenced_code', 'codehilite']))
+
 @app.route('/reset')
 def reset():
     global messages
     messages = []
     return "Conversation history has been reset."
+
+@app.route('/history', methods = ["GET"])
+def history():
+    user_ip = request.remote_addr
+
+    cursor = connection.cursor()
+    search_query = "SELECT * FROM user_info WHERE ip = %s"
+    cursor.execute(search_query, user_ip)
+    result = cursor.fetchall()
+
+    cursor.close()
+    return render_template('history.html', result_tuple = result)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
